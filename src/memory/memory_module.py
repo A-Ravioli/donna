bdf00f9f4d3ad9a653bcdc141a2aacff22b3a98c
Arav Kumar <68564296+A-Ravioli@users.noreply.github.com>
2025-03-06 08:17:58 -0800

    starting over

--- src/memory/memory_module.py
+++
@@ -1,7 +0,0 @@
-from langchain.memory import ConversationBufferMemory
-
-
-def get_memory():
-    # Return a simple conversation memory to store chat history.
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    return memory 
